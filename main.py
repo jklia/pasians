006d4ad7683fca8be0c45a0a3af06feb91082fa7
jklia <145899252+jklia@users.noreply.github.com>
2023-10-08 13:03:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -19,19 +19,22 @@ class Card(sprite.Sprite):
     def __init__(self, x_co, y_co, width, height, opened, front, back, kind, numb):
         super().__init__()
         # каждый спрайт должен хранить свойство image - изображение
-        self.image = Surface((width, height))
-        self.image.fill((0, 0, 0))
         self.kind = kind
         self.numb = numb
-        if opened == 1:
-            self.image = transform.scale(image.load(front), (width, height))
-        elif opened == 2:
-            self.image = transform.scale(image.load(back), (width, height))
+        self.front = front
+        self.back = back
+        self.width = width
+        self.height = height
+        self.image = transform.scale(image.load(back), (width, height))
         # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
         self.rect = self.image.get_rect()
         self.rect.x = x_co
         self.rect.y = y_co
-
+    def change (self, opened):
+        if opened == 1:
+            self.image = transform.scale(image.load(self.front), (self.width, self.height))
+        elif opened == 2:
+            self.image = transform.scale(image.load(self.back), (self.width, self.height))
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
@@ -46,6 +49,9 @@ window = display.set_mode((win_width, win_height))
 # background = transform.scale(image.load("bosh.png"), (win_width, win_height))
 window.fill((200, 255, 255))
 
+image1 = transform.scale(image.load('card_back.png'), (90, 120))
+window.blit(image1, (800, 200))
+
 field = [
     [],
     [],
@@ -59,18 +65,26 @@ field = [
 cards = []
 for i in range(4):
     for j in range(13):
-        card = Card(800, 200, 90, 120, 1, pictures[i][j], 'card_back.png', kinds[i], j+1)
+        card = Card(800, 200, 90, 120, 2, pictures[i][j], 'card_back.png', kinds[i], j+1)
         cards.append(card)
 
 random.shuffle(cards)
-column = cards
+column = cards.copy()
+column1 = []
+#lying_cards = []
+k = 0
 for i in range(7):
     for j in range(i + 1):
-        cards[j].rect.x = i * 100
-        cards[j].rect.y = j * 70
-        field[i].append(cards[j])
-        column.pop(j)
-
+        print(k)
+        cards[k].rect.x = i * 100
+        cards[k].rect.y = j * 70
+        cards[k].change(1)
+        field[i].append(cards[k])
+        #lying_cards.append(cards[j])
+        column.pop(0)
+        k += 1
+for card in cards:
+    print(card.rect.x, card.rect.y)
 game = True
 finish = False
 clock = time.Clock()
@@ -87,47 +101,53 @@ active = None
 while game:
 
     window.fill((200, 255, 255))
-    column[k].reset()
+    column[k].change(1)
+    for card in column:
+        card.reset()
     for i in range(7):
         for card in (field[i]):
             card.reset()
-
     for e in event.get():
         if e.type == QUIT:
             game = False
         if e.type == MOUSEBUTTONDOWN:
             if e.button == 1:
+                print("eeeee")
                 xm, ym = e.pos
-                if xm > 800 and xm < 890 and ym > 200 and ym < 320:
+                if 800 < xm < 890 and 200 < ym < 320:
                     if k < 23:
                         k += 1
                     elif k == 23:
                         k = 0
-            if e.button == 2:
+                if column[k].rect.x < 900:
+                    column[k].rect.x = column[k].rect.x + 100
+                else:
+                    for card in column:
+                        card.rect.x = 800
+
+            if e.button == 3:
+                print("sssssss")
                 xm, ym = e.pos
                 for card in cards:
                     if card.inside(xm, ym):
+                        print("vvvvvv")
                         active = cards.index(card)
 
         if e.type == MOUSEMOTION:
-            if active != None:
-                cards[active].move_ip(e.rel)
+            xm, ym = e.pos
+            if active is not None:
+                print("aaaaaa")
+                cards[active].rect.centerx = xm
+                cards[active].rect.centery = ym
 
         if e.type == MOUSEBUTTONUP:
-            if e.button == 2:
+            if e.button == 3:
                 active = None
     #window.fill((200, 255, 255))
     # column[k].reset()
     # for i in range(7):
     #     for card in (field[i]):
     #         card.reset()
-
+    window.blit(image1, (800, 200))
     display.update()
     clock.tick(FPS)
-
-
-
-
-
-
-
